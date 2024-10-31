bd9d195dde19b840b2c1e69f5d9448f58b87e25a
dogTaksa <gaisrininkas112@gmail.com>
2024-10-31 17:42:30 +0200

    B-ver

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +8,7 @@ public class Player : MonoBehaviour
 {
     public GameObject[] levels;
     public int i = 0;
+    public bool tpToStart = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,16 +27,20 @@ public class Player : MonoBehaviour
         //    Cursor.visible = false;
         //}
 
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = mousePos;
+        if (!tpToStart)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = mousePos;
+        }
+
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         print("Collision!");
-        if (collision.gameObject.name == "Teleporter")
+        if (collision.gameObject.name.Contains("Teleporter"))
         {
-            if (i != 2)
+            if (i != 2 && i != 3)
             {
                 levels[i].gameObject.SetActive(false);
                 levels[i + 1].gameObject.SetActive(true);
@@ -49,19 +55,42 @@ public class Player : MonoBehaviour
                 levels[3].gameObject.SetActive(true);
                 GetComponent<AudioSource>().Play();
                 i = 0;
+
+                Thread.Sleep(7000);
+                SceneManager.LoadScene("SampleScene");
             }
         }
         else
         {
-            levels[0].gameObject.SetActive(false);
-            levels[1].gameObject.SetActive(false);
-            levels[2].gameObject.SetActive(false);
+            if (i != 2)
+            {
+                levels[0].gameObject.SetActive(true);
+                levels[1].gameObject.SetActive(false);
+                levels[2].gameObject.SetActive(false);
+                levels[3].gameObject.SetActive(false);
 
-            levels[3].gameObject.SetActive(true);
-            GetComponent<AudioSource>().Play();
-            i = 0;
-            SceneManager.LoadScene("SampleScene");
+                i = 0;
+
+                transform.position = new Vector2(-7f, -4f);
+                tpToStart = !tpToStart;
+            }
+            else
+            {
+                levels[0].gameObject.SetActive(false);
+                levels[1].gameObject.SetActive(false);
+                levels[2].gameObject.SetActive(false);
+
+                levels[3].gameObject.SetActive(true);
+                GetComponent<AudioSource>().Play();
+                i = 0;
+            }
+
         }
         
+    }
+
+    private void OnMouseOver()
+    {
+        tpToStart = false;
     }
 }
